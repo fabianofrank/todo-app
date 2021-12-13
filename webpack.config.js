91e97b5fc3d8857af816602d01b012387c178f2f
fabianofrank <fabianofrank@protonmail.com>
2021-12-13 13:55:41 -0300

    Add publicPath:/todo-app/

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: "/dist/",
+    publicPath: "/todo-app/",
   },
   module: {
     rules: [
