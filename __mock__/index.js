4d0b3b86a4a65f7dbda416ed6c4e636ff663cfe7
fabianofrank <fabianofrank@protonmail.com>
2021-12-16 16:15:54 -0300

    Add localStorage class

--- __mock__/index.js
+++ __mock__/index.js
@@ -42,25 +42,30 @@ const checkItem = (item) => {
   item.completed = !item.completed;
 };
 
-const localStorage = (function() {
-  var store = {};
-  return {
-    getItem: function(key) {
-      return store[key];
-    },
-    setItem: function(key, value) {
-      store[key] = value.toString();
-    },
-    clear: function() {
-      store = {};
-    },
-    removeItem: function(key) {
-      delete store[key];
-    }
-  };
-})();
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
 
-// Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock;
+
 const setStored = () => localStorage.setItem('newItems', JSON.stringify(items));
 const getStored = () => JSON.parse(localStorage.getItem('newItems'));
 
